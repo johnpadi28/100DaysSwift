54c00b710586147968ca055a6e733b15cfcd2635
John Padilla <johnpadi28@gmail.com>
2020-10-13 10:24:49 -0600

    Day33

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,8 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by John Padilla on 10/13/20.
+//
+
+import Foundation
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,8 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by John Padilla on 10/13/20.
+//
+
+import Foundation
