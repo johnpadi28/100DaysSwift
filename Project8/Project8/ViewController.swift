8184affba7e7da37f15ba3973ceea3faef911d49
John Padilla <johnpadi28@gmail.com>
2020-10-17 23:31:35 -0600

    Day36

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by John Padilla on 10/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
