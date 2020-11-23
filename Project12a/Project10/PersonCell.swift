a2471ca87b7141f3610a8b9a5dcd61c9c66e3f10
John Padilla <johnpadi28@gmail.com>
2020-11-23 12:05:45 -0700

    Day48

---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by John Padilla on 11/2/20.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+
+}
